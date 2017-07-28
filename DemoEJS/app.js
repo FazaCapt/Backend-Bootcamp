6de9c61211d4c7e0bbcc5ecc3c90e4a28d96741a
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-07-28 07:54:37 +0700

    EJS: Conditionals and Loops

--- DemoEJS/app.js
+++ DemoEJS/app.js
@@ -10,6 +10,15 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "Post 2", author: "Budy"},
+        {title: "Post 3", author: "Tono"}
+    ]
+    res.render("posts.ejs", {posts: posts});
+});
+
 app.listen(3000, function(){
     console.log("Server is listening!!!");
 });
