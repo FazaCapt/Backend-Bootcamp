1f82de943b5a37e49ab8fe34df8ed583f6eed286
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-07-31 08:44:38 +0700

    Movie API App: Adding Search

--- movie_app/app.js
+++ movie_app/app.js
@@ -4,11 +4,15 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/search", function(req,res){
+    res.render("search");
+})
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
-        if(!error && response.statusCode == 200){
-            // var results = JSON.parse(body)
-            // res.send(results["Search"][0]["Title"]);
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s="+ query + "&apikey=thewdb"
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){        
             var data = JSON.parse(body)
             res.render("results", {data:data});
         }
